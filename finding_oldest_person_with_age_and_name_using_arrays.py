e5d431b084dcbb335f36f7a9b709b7c40f40ce5e
unknown <jaredtiu1706@gmail.com>
2024-10-21 01:32:26 +0800

    added valid ages and printing of array

--- finding_oldest_person_with_age_and_name_using_arrays.py
+++ finding_oldest_person_with_age_and_name_using_arrays.py
@@ -1,19 +1,26 @@
+person_age_and_name = {}
 #asking for user input 
-for i in range(2): 
+while True: 
+    #setting a definition of a valid name and age
     try:  
         name = input("Input a name: ") 
-
+            
         while True: 
-            age = int(input("input a age: "))
+            age = input("Input a age: ")
 
-            if len(age) != 2: 
-                print("INVALID")
+            if len(age) != 2 or age != 1: 
+                break
+
+        #get the info into an array     
+        person_age_and_name = {
+            "name": name,
+            "age": age
+        }
+        print(person_age_and_name ["name"])
+        print(person_age_and_name ["age"])
+
     except:
-        print("Invalid") 
-        
-
-#setting a definition of a valid name and age 
-
+        print("INVALID")    
 #get the info into an array 
 
 #ask if the user wants to continue with inputing age and names 
