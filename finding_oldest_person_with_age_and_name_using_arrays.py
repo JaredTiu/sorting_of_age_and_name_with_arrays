79e21dec5044fd8619d8535fba8926416b4d4947
unknown <jaredtiu1706@gmail.com>
2024-10-21 00:51:57 +0800

    asking for user input

--- finding_oldest_person_with_age_and_name_using_arrays.py
+++ finding_oldest_person_with_age_and_name_using_arrays.py
@@ -1,4 +1,16 @@
 #asking for user input 
+for i in range(2): 
+    try:  
+        name = input("Input a name: ") 
+
+        while True: 
+            age = int(input("input a age: "))
+
+            if len(age) != 2: 
+                print("INVALID")
+    except:
+        print("Invalid") 
+        
 
 #setting a definition of a valid name and age 
 
