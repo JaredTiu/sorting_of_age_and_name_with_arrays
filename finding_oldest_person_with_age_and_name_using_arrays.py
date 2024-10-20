5b40253b8481bd8361c73378e3d7bd533b1277a4
unknown <jaredtiu1706@gmail.com>
2024-10-21 00:33:02 +0800

    added pseudocode

---
+++ finding_oldest_person_with_age_and_name_using_arrays.py
@@ -0,0 +1,10 @@
+#asking for user input 
+
+#setting a definition of a valid name and age 
+
+#get the info into an array 
+
+#ask if the user wants to continue with inputing age and names 
+
+#if they say no display the name and age of the oldest person
+
