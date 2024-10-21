b6822913830660ad33758edcb31275d420baf028
unknown <jaredtiu1706@gmail.com>
2024-10-21 11:28:51 +0800

    set the len value of the age

--- finding_oldest_person_with_age_and_name_using_arrays.py
+++ finding_oldest_person_with_age_and_name_using_arrays.py
@@ -2,28 +2,32 @@ person_age_and_name = {}
 #asking for user input 
 while True: 
     #setting a definition of a valid name and age
-    try:  
-        name = input("Input a name: ") 
+    try: 
+        while True:
+            name = input("Input a name: ") 
             
-        while True: 
-            age = input("Input a age: ")
-
-            if len(age) != 2 or age != 1: 
+            if name != int:
+                print("Input a valid name: ")
+            elif name == str:
                 break
+                
+            while True: 
+                age = input("Input a age: ")
 
-        #get the info into an array     
-        person_age_and_name = {
-            "name": name,
-            "age": age
-        }
-        print(person_age_and_name ["name"])
-        print(person_age_and_name ["age"])
+                if len(age) == 2: 
+                    break
+                elif len(age) == 1: 
+                    break
 
+            #get the info into an array     
+            person_age_and_name = {
+                "name": name,
+                "age": age
+            }
+            print(person_age_and_name ["name"])
+            print(person_age_and_name ["age"])
     except:
         print("INVALID")    
-#get the info into an array 
-
 #ask if the user wants to continue with inputing age and names 
 
 #if they say no display the name and age of the oldest person
-
