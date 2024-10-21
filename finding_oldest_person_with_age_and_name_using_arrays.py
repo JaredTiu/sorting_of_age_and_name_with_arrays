3749e1b2eaa912db64f740dfbad50a0d567ba36a
unknown <jaredtiu1706@gmail.com>
2024-10-21 18:12:05 +0800

    improved the retry question, added some breaks. higher value locator still not working

--- finding_oldest_person_with_age_and_name_using_arrays.py
+++ finding_oldest_person_with_age_and_name_using_arrays.py
@@ -1,4 +1,5 @@
 default_highest_age = -1
+name_of_person_with_highest_age = ""
 #making a for loop to find the biggest number in the array 
 person_name_and_age = {}
     #asking for user input 
@@ -9,7 +10,7 @@ while True:
             name = input("Input a name: ")
 
             while True: 
-                age = input("Input a age: ")
+                age = int(input("Input a age: "))
 
                 if age < 0 or age > 100: 
                     raise
@@ -22,16 +23,17 @@ while True:
             }        
             
             #ask if the user wants to continue with inputing age and names
-            retry = input("Do you want to continue: ")
+            retry = input("Do you want to continue, yes or no? : ")
             break
         except:
             print("INVALID")
 #if they say no display the name and age of the oldest person
     if retry == "no":
-       for user_age, user_name in person_name_and_age.items(): 
+       for user_name, user_age in person_name_and_age.items(): 
            if user_age > default_highest_age:
-            default_highest_age = user_age
-            name_of_person_with_highest_age = user_name
-            print(f"{name_of_person_with_highest_age} has the highest age, they are  {default_highest_age} years old." )
+                default_highest_age = user_age
+                name_of_person_with_highest_age = user_name
+                print(f"{name_of_person_with_highest_age} has the highest age, they are  {default_highest_age} years old." )
     elif retry != "yes":
         print("Invalid")
+        break
