36ccdb7d958d34f45163834f497e359c11b28e90
unknown <jaredtiu1706@gmail.com>
2024-10-22 14:37:17 +0800

    added a special character checker, not working yet

--- finding_oldest_person_with_age_and_name_using_arrays.py
+++ finding_oldest_person_with_age_and_name_using_arrays.py
@@ -1,15 +1,30 @@
+#setting more definitions for valid name
+def valid_name(name):
+    for character in name:
+        if not (character.isalpha() or character.isdigit() or character == ""):
+            return False
+    return True
+
 #making a for loop to find the biggest number in the array 
 person_name_and_age = {}
     #asking for user input 
 while True:
     while True: 
     #setting a definition of a valid name and age
-        try: 
             name = input("Input a name: ")
 
-            while True: 
-                age = int(input("Input a age: "))
+            while not valid_name(name):
+                print("Do not include Special characters")
+                name = input("Enter a name without special characters: ")
 
+            while True:
+                try: 
+                    age = int(input("Input a age: "))
+                except:
+                    print("Enter only numbers ")
+                    age = int(input("Input a valid age: "))
+
+                    
                 if age < 0 or age > 100: 
                     raise 
                 break
@@ -18,20 +33,18 @@ while True:
             #ask if the user wants to continue with inputing age and names
             retry = input("Do you want to continue, yes or no? : ")
             break
-        except:
-            print("INVALID")
-#if they say no display the name and age of the oldest person
+    #if they say no display the name and age of the oldest person
     if retry == "no":
 
         highest_value = max(person_name_and_age.values())
         highest_names = []
 
-        for key in person_name_and_age: 
-            if person_name_and_age[key] == highest_value: 
-                highest_names.append(key)
-        print(f"The person who is the oldest is: {highest_names} They are {highest_value} years old.")
+        for value in person_name_and_age: 
+            if person_name_and_age[value] == highest_value: 
+                highest_names.append(value)
+        print(f"The person who is the oldest is: {highest_names} they are {highest_value} years old.")
         break
-    
+
     elif retry != "yes":
         print("Invalid")
         break
